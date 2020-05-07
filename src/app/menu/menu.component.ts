a63369cdb987623ada3bd950f8f42bdfc9afd86a
Jonathan Tapin <jonathan@tapin.com>
2020-05-07 14:51:42 +0200

    USGDM003-#5 #4 #6 - Menu Empployé, Manager et Administrateur fonctionnel et responsive. Le menu se modifie en fonction des rôles attribués à l'utilisateur connecté. La barre de navigation est responsive.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -4,6 +4,7 @@ import { AuthService } from './../auth/auth.service';
 import { Collegue } from './../auth/auth.domains';
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
@@ -24,25 +25,9 @@ export class MenuComponent implements OnInit {
     );
   }
 
-  admin = false
-  manager = false
-  role(){
-    this.collegueConnecte.subscribe(
-      col => {
-        col.roles.forEach(element => {
-          if (element === `ROLE_ADMINISTRATEUR`) {
-         this.admin = true
-        } else if (element === `ROLE_MANAGER`) {
-          this.manager = true
-        }
-      });
-    }
-  )
-}
-
   ngOnInit(): void {
     this.collegueConnecte = this.authSrv.collegueConnecteObs;
-    this.role()
+
   }
 
 }
